e3f7091c50e0a40a05137e4a6ecaf2c6ef02e4a4
vicveloso <victoriaveferreira@gmail.com>
2021-11-09 21:11:08 -0300

    aula ides

---
+++ src/br/com/dio/primeiroProjeto.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroProjeto {
+    public static void main(String[] args) {
+        gato animal = new gato( );
+        System.out.println(animal);
+//        int a = 10;
+//        int b = 22;
+//        System.out.println("Hello World!" + ("\n")+(a+b));]
+    }
+}
